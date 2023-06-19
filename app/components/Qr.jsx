4a1ff5e7fbb8eedabb35f69a19c33ede1c8c45b2
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-19 08:19:21 +0530

    updated Qr component

--- app/components/Qr.jsx
+++ app/components/Qr.jsx
@@ -1,20 +1,40 @@
+'use client'
+
 import QRCode from "react-qr-code";
+import { useState } from "react";
 
 const Qr = () => {
+  const [qrValue, setQrValue] = useState("Nothing to show");
   return (
-    <section className=" w-48 h-48 bg-white border-2 border-slate-950 my-10 rounded-md p-1">
-        <QRCode 
-            className="rounded-md"
-            value="hi" 
-            size={180} 
-            bgColor="#FFFFFF"
-            fgColor="#000000"
-            level="Q"
-            includeMargin={true}
-            renderAs="svg"
+    <section className="flex flex-col items-center w-64 ">
+      <div className="w-48 h-48 bg-white border-2 border-slate-950 
+                      mt-10 rounded-md p-1">
+        <QRCode
+          className="rounded-md"
+          value={qrValue}
+          size={180}
+          bgColor="#FFFFFF"
+          fgColor="#000000"
+          level="Q"
+          includeMargin={true}
+          renderAs="svg"
         />
+      </div>
+      <div className="flex flex-col mt-5 w-64">
+        <label className="text-slate-950 font-semibold text-md mb-1"
+                htmlFor="qrText"
+        >
+          Enter the content
+        </label>
+        <input className="h-8 border-2 border-slate-950 rounded-md p-2" 
+                type="text"
+                id="qrText"
+                placeholder="Enter the content..."
+                onChange={(e) => setQrValue(e.target.value)}
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Qr
+export default Qr;
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'QR Code Generator',
-  description: 'Web app to generate QR codes',
-}
+  title: "QR Code Generator",
+  description: "Web app to generate QR codes",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=' h-screen flex flex-col justify-start align-middle pt-10'>
-        <h1 className=' text-3xl font-bold mx-auto'>QR Generator</h1>
+      <body className=" h-screen flex flex-col justify-start align-middle pt-10">
+        <h1 className=" text-3xl font-bold mx-auto">QR Generator</h1>
         {children}
       </body>
     </html>
-  )
+  );
 }
