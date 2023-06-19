e4e8aa42da0625decd79d03a72047be759028b73
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-19 08:20:30 +0530

    applied prettier

--- app/components/Qr.jsx
+++ app/components/Qr.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import QRCode from "react-qr-code";
 import { useState } from "react";
@@ -7,8 +7,10 @@ const Qr = () => {
   const [qrValue, setQrValue] = useState("Nothing to show");
   return (
     <section className="flex flex-col items-center w-64 ">
-      <div className="w-48 h-48 bg-white border-2 border-slate-950 
-                      mt-10 rounded-md p-1">
+      <div
+        className="w-48 h-48 bg-white border-2 border-slate-950 
+                      mt-10 rounded-md p-1"
+      >
         <QRCode
           className="rounded-md"
           value={qrValue}
@@ -21,16 +23,18 @@ const Qr = () => {
         />
       </div>
       <div className="flex flex-col mt-5 w-64">
-        <label className="text-slate-950 font-semibold text-md mb-1"
-                htmlFor="qrText"
+        <label
+          className="text-slate-950 font-semibold text-md mb-1"
+          htmlFor="qrText"
         >
           Enter the content
         </label>
-        <input className="h-8 border-2 border-slate-950 rounded-md p-2" 
-                type="text"
-                id="qrText"
-                placeholder="Enter the content..."
-                onChange={(e) => setQrValue(e.target.value)}
+        <input
+          className="h-8 border-2 border-slate-950 rounded-md p-2"
+          type="text"
+          id="qrText"
+          placeholder="Enter the content..."
+          onChange={(e) => setQrValue(e.target.value)}
         />
       </div>
     </section>
