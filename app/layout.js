1f98a1868e5229d424624b2784a0f2c960375c4c
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-20 08:33:02 +0530

    rearranged icons and UI

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=" h-screen flex flex-col justify-start align-middle pt-10">
+      <body className=" h-screen flex flex-col justify-start align-middle pt-5">
         <h1 className=" text-3xl font-bold mx-auto">QR Generator</h1>
         {children}
       </body>
